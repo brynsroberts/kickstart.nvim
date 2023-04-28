cc4a0e01d503dc37407f6e201725354a73b82ca3
Bryan Roberts <roberts@amyris.com>
2023-04-28 12:58:41 -0700

    ADD IT BACK

--- init.lua
+++ init.lua
@@ -178,6 +178,7 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
